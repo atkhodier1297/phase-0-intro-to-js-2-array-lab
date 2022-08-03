f08a988586c6df0c381b21b835fa397f6cb054d0
Adam Khodier <khodieradam1297@gmail.com>
2022-08-03 15:42:13 -0400

    commit tree 4189368bf35b112a6ac80cd8bea0eac729267cf3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+const catss = ["Milo" , "Otis", "Garfield"];
+
+function logCats(catss) {
+    console.log ("The cats in order are", catss);
+}
+
+const cats = ["Milo" , "Otis", "Garfield"];
+function addCat(cats) {
+    console.log (cats.push("Ralph"), cats)
+}
+
+
